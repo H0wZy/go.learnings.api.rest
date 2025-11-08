f16b9c0be1f68d140fc657c84ae87b9f8b50b9c5
h0wzy <h0wzymarcos@gmail.com>
2025-11-08 14:43:16 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/H0wZy/go.learnings.restapi/router"
+import (
+	"github.com/H0wZy/go.learnings.restapi/config"
+	"github.com/H0wZy/go.learnings.restapi/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.InitDb()
+	if err != nil {
+		logger.Infof("config init error: %v", err)
+		return
+	}
 	router.Initialize()
 }

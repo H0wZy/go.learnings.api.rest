d402539b0f3afdacd01ecee46031510346ca206e
h0wzy <h0wzymarcos@gmail.com>
2025-11-08 17:42:43 -0300

    feat: add request and response helpers

---
+++ handler/response.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sendError(ctx *gin.Context, code int, success bool, msg string) {
+	ctx.Header("Content-Type", "application/json; charset=utf-8")
+	ctx.JSON(code, gin.H{
+		"statusCode": code,
+		"success":    success,
+		"message":    msg,
+	})
+}
+func sendSuccess(ctx *gin.Context, opr string, data interface{}, success bool) {
+	ctx.Header("Content-Type", "application/json; charset=utf-8")
+	ctx.JSON(http.StatusOK, gin.H{
+		"statusCode": http.StatusOK,
+		"success":    success,
+		"message":    fmt.Sprintf("operation from handler: %s successfull", opr),
+		"data":       data,
+	})
+}
